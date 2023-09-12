732d80c3ac7b135115351007fdf0373a997e4372
Joe Davis <jdavis@ourmagnet.net>
2023-09-11 20:52:05 -0500

    moving asteroids with polar vectors

--- Assets/Scripts/Asteroid.cs
+++ Assets/Scripts/Asteroid.cs
@@ -8,11 +8,155 @@ public class Asteroid : MonoBehaviour {
     List<Sprite> spriteListA;
     List<Sprite> spriteListB;
     float elapsedTime;
+    
     int frame;
+    public int Frame {
+        get {
+            return frame;
+        }
+        set {
+            frame = value;
+        }
+    }
 
-    // object props
-    int framesPerSecond = 16;
-    int animationType = 1;
+    int framesPerSecond;
+    public int FramesPerSecond {
+        get {
+            return framesPerSecond;
+        }
+        set {
+            framesPerSecond = value;
+        }
+    }
+
+    int animationType;
+    public int AnimationType {
+        get {
+            return animationType;
+        }
+        set {
+            animationType = value;
+        }
+    }
+
+    float xPosition;
+    public float XPosition {
+        get {
+            return xPosition;
+        }
+        set {
+            xPosition = value;
+            Vector3 position = this.gameObject.transform.position;
+            position.x = value;
+            transform.position = position;
+        }
+    }
+
+    float yPosition;
+    public float YPosition {
+        get {
+            return yPosition;
+        }
+        set {
+            yPosition = value;
+            Vector3 position = this.gameObject.transform.position;
+            position.y = value;
+            transform.position = position;
+        }
+    }
+
+    float velocity;
+    public float Velocity {
+        get {
+            return velocity;
+        }
+        set {
+            velocity = value;
+            UpdateComponentVelocitiesFromPolarVector();
+        }
+    }
+
+    float trajectory;
+    public float Trajectory {
+        get {
+            return trajectory;
+        }
+        set {
+            trajectory = value;
+            UpdateComponentVelocitiesFromPolarVector();
+        }
+    }
+
+    float xVelocity;
+    public float XVelocity {
+        get {
+            return xVelocity;
+        }
+        set {
+            xVelocity = value;
+        }
+    }
+
+    float yVelocity;
+    public float YVelocity {
+        get {
+            return yVelocity;
+        }
+        set {
+            yVelocity = value;
+        }
+    }
+
+    float size;
+    public float Size {
+        get {
+            return size;
+        }
+        set {
+            size = value;
+            transform.localScale = new Vector3(size, size, 1f);
+        }
+    }
+
+    float rotation;
+    public float Rotation {
+        get {
+            return rotation;
+        }
+        set {
+            rotation = value;
+            transform.localEulerAngles = new Vector3(1f, 1f, rotation);
+        }
+    }
+
+    void UpdateComponentVelocitiesFromPolarVector() {
+        xVelocity = Mathf.Cos(trajectory * Mathf.Deg2Rad) * velocity;
+        yVelocity = Mathf.Sin(trajectory * Mathf.Deg2Rad) * velocity;
+    }
+
+    public void AdvancePosition(float delta) {
+        xPosition += xVelocity * delta;
+        yPosition += yVelocity * delta;
+        Vector3 position = this.gameObject.transform.position;
+        position.x = xPosition;
+        position.y = yPosition;
+        transform.position = position;
+    }
+
+    // Constructor
+    public Asteroid(int initialValue)
+    {
+        elapsedTime = 0;
+        frame = 0;
+        framesPerSecond = 16;
+        animationType = 0;
+        velocity = 0;
+        trajectory = 0;
+        xVelocity = 0;
+        yVelocity = 0;
+        size = 1;
+        rotation = 0;
+    }
 
     void Start()
     {
@@ -20,16 +164,8 @@ public class Asteroid : MonoBehaviour {
         Main script = GameObject.Find("Game").GetComponent<Main>();
         spriteListA = script.asteroidSpriteListA;
         spriteListB = script.asteroidSpriteListB;
-        elapsedTime = 0;
-        frame = 0;
         spriteRenderer.sprite = spriteListA[frame];
-        if (animationType == 0) {
-            spriteRenderer.sprite = spriteListA[frame];
-        } else {
-            spriteRenderer.sprite = spriteListB[frame];
-        }
     }
-
     
     void Update()
     {
--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -4,36 +4,80 @@ using UnityEngine;
 
 public class Main : MonoBehaviour
 {
-    [SerializeField] Sprite spriteMap;
+    [SerializeField] Sprite asteroidSpriteMap;
+    [SerializeField] GameObject asteroidPrefab;
 
     public List<Sprite> asteroidSpriteListA;
     public List<Sprite> asteroidSpriteListB;
 
-    Texture2D texture;
+    Texture2D asteroidTexture;
+    List<Asteroid> asteroids;
 
-    void Start()
-    {
-        texture = spriteMap.texture;
+    Sprite GenerateAsteroidSpriteFrame(int x, int y) {
+        Rect rect = new Rect(x * 128, y * 128, 128, 128);
+        Sprite sprite = Sprite.Create(asteroidTexture, rect, Vector2.one * 0.5f);
+        return sprite;
+    }
+
+    void GenerateSprites() {
+        asteroidTexture = asteroidSpriteMap.texture;
         for (int y = 3; y >= 0; y--) {
             for (int x = 0; x <= 7; x++) {
-                asteroidSpriteListA.Add(generateSpriteFrame(x, y));
+                asteroidSpriteListA.Add(GenerateAsteroidSpriteFrame(x, y));
             }
         }
         for (int y = 7; y >= 4; y--) {
             for (int x = 0; x <= 7; x++) {
-                asteroidSpriteListB.Add(generateSpriteFrame(x, y));
+                asteroidSpriteListB.Add(GenerateAsteroidSpriteFrame(x, y));
             }
         }
     }
 
-    Sprite generateSpriteFrame(int x, int y) {
-        Rect rect = new Rect(x * 128, y * 128, 128, 128);
-        Sprite sprite = Sprite.Create(texture, rect, Vector2.one * 0.5f);
-        return sprite;
+    void CreateAsteroid() {
+        Asteroid asteroid = Instantiate(asteroidPrefab).GetComponent<Asteroid>();
+        asteroid.FramesPerSecond = Random.Range(8, 33);
+        asteroid.AnimationType = Random.Range(0, 2);
+        asteroid.XPosition = Random.Range(-10f, 11f);
+        asteroid.YPosition = Random.Range(-10f, 11f);
+        asteroid.Velocity = Random.Range(0.25f, 3f);
+        asteroid.Trajectory = Random.Range(0f, 365f);
+        asteroid.Size = Random.Range(0.25f, 2f);
+        asteroid.Rotation = Random.Range(-45f, 45f);
+
+        asteroids.Add(asteroid);
     }
+
+    void Start()
+    {
+        asteroids = new List<Asteroid>();
+        GenerateSprites();
+        // for (int i = 0; i < 100; i++) {
+        //     CreateAsteroid();
+        // }
+        CreateAsteroid();
+        CreateAsteroid();
+        asteroids[0].XPosition = -3f;
+        asteroids[0].YPosition = -3f;
+        asteroids[0].Size = 0.75f;
+        asteroids[0].Trajectory = 40f;
+        asteroids[0].Velocity = 0.5f;
+
+        asteroids[1].XPosition = 2f;
+        asteroids[1].YPosition = 5f;
+        asteroids[1].Size = 0.3f;
+        asteroids[1].Trajectory = 250f;
+        asteroids[1].Velocity = 0.7f;
+
+
+    }
+
+    
 
     void Update()
     {
-        
+        float delta = Time.deltaTime;
+        foreach(Asteroid ast in asteroids) {
+            ast.AdvancePosition(delta);
+        }
     }
 }
